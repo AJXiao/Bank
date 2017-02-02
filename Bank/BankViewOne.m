c33db73614d33dadb4221b5a68dc17a4774d4d72
AJMD <panxr@knowbox.cn>
2017-02-02 14:12:37 +0800

    提交文件

---
+++ Bank/BankViewOne.m
@@ -0,0 +1,25 @@
+//
+//  BankViewOne.m
+//  cocopod私有库制作
+//
+//  Created by AJMD on 17/2/2.
+//  Copyright © 2017年 AJMD. All rights reserved.
+//
+
+#import "BankViewOne.h"
+#import "AFHTTPSessionManager.h"
+
+@implementation BankViewOne
+
+
+- (void)test{
+    NSLog(@"TEST");
+}
+
+- (void)server_requestData{
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc]init];
+    NSLog(@"%@",manager);
+}
+
+
+@end
